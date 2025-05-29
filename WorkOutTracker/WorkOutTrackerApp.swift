54c6db5421dddc2f6448b5ffb416fdbc45d9cbb4
Bryan Galaviz <bleog22@gmail.com>
2025-05-29 17:35:29 -0500

    Initial Commit

---
+++ WorkOutTracker/WorkOutTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorkOutTrackerApp.swift
+//  WorkOutTracker
+//
+//  Created by Bryan Galaviz on 5/29/25.
+//
+
+import SwiftUI
+
+@main
+struct WorkOutTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
